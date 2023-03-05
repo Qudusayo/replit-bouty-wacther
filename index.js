0ef091c30971305a0421f3356793cff84c4e0790
Qudusayo <qqudusayo@gmail.com>
2023-03-05 09:09:06 +0100

    fix: moved to express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+// Path to the scheduled function(s)
+const scheduledFunctions = require("./scheduledFunctions");
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+// ADD CALL to execute your function(s)
+scheduledFunctions.initScheduledJobs();
+
+app.listen(app.get("port"), () => {
+  console.log("Express server listening on port " + app.get("port"));
+});
